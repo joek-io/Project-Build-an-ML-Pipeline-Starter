1b551cb456ad34dac57b9eef98a0ce1ac0464c50
Joseph Kuykendall <56773616+joek-io@users.noreply.github.com>
2025-10-05 16:53:17 -0400

    Add test_regression_model to main.py

--- main.py
+++ main.py
@@ -122,11 +122,15 @@ def go(config: DictConfig):
 
         if "test_regression_model" in active_steps:
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+           _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
+                "main",
+                parameters={
+                    "mlflow_model": config["modeling"]["output_artifact"] + ":prod",
+                    "test_dataset": "test_data.csv:latest"
+                }
+            )
+        pass
 
 
 if __name__ == "__main__":
